b16a4ae46a787e4d48e95d5b8702590769446cda
Loic Rossi <l.c.g.rossi@tudelft.nl>
2017-03-01 18:04:20 +0100

    Added wavelength dependence
    added wavelength dependence in exopy_radiance in the integration function
    but this implies that the output for the grid and stokes elements stored in the
    body object now have a extra dimension

--- exopy_radiance.py
+++ exopy_radiance.py
@@ -56,15 +56,18 @@ def integration(body):
     #files = dict(np.genfromtxt('exopy/scenes.dat', dtype='str'))
 
     time = body.ephemeris.time
-    body.grid.I = np.zeros_like(body.grid.shadow)
-    body.grid.Q = np.zeros_like(body.grid.shadow)
-    body.grid.U = np.zeros_like(body.grid.shadow)
-    body.grid.V = np.zeros_like(body.grid.shadow)
+    wvl_list = body.atmosphere.wvl_list
+    nwvl = len(wvl_list)
 
-    Ip = np.zeros_like(time)
-    Qp = np.zeros_like(time)
-    Up = np.zeros_like(time)
-    Vp = np.zeros_like(time)
+    body.grid.I = np.zeros_like((nwvl,body.grid.shadow))
+    body.grid.Q = np.zeros_like((nwvl,body.grid.shadow))
+    body.grid.U = np.zeros_like((nwvl,body.grid.shadow))
+    body.grid.V = np.zeros_like((nwvl,body.grid.shadow))
+
+    Ip = np.zeros_like((nwvl,time))
+    Qp = np.zeros_like((nwvl,time))
+    Up = np.zeros_like((nwvl,time))
+    Vp = np.zeros_like((nwvl,time))
 
     area  = np.repeat(body.grid.area[:,np.newaxis],4,1).T
     phase = np.degrees(body.geometry.phase_angle)
@@ -75,72 +78,41 @@ def integration(body):
 
     scene = body.properties.fourier_scene
 
-    #for now l is 0
-    l=0
 
-    for i,j in enumerate(time):
+    for l,wvl in wvl_list:
+        for i,j in enumerate(time):
 
-        print i+1, ' out of ', len(time)
-        A = body.grid.shadow[i,:]>10E-10
+            print i+1, ' out of ', len(time)
+            A = body.grid.shadow[i,:]>10E-10
 
-        IQUV = np.zeros([4,body.grid.N_points])
+            IQUV = np.zeros([4,body.grid.N_points])
 
-        ######################################################################################
-        #######################       PMD.READ_DAP_OUTPUT      ###############################
-        ######################################################################################
+            ######################################################################################
+            #######################       PMD.READ_DAP_OUTPUT      ###############################
+            ######################################################################################
 
-        I,Q,U,V = pmd.read_dap_output(np.repeat(phase[i], sum(A)), sza[i,A],
-                                      emission[A], body.atmosphere.name[l],
-                                      beta=beta[i,A],
-                                      phi=phi[i,A])
+            I,Q,U,V = pmd.read_dap_output(np.repeat(phase[i], sum(A)), sza[i,A],
+                                        emission[A], body.atmosphere.name[l],
+                                        beta=beta[i,A],
+                                        phi=phi[i,A])
 
-        ######################################################################################
-        ##########################       GEOS.READ_DAP      ##################################
-        ######################################################################################
-        '''											##
-            ngeos = sum(A)									##
-                                                ##
-            #Preparing vectors for FORTRAN function						##
-            phaseF     = np.zeros(ngeosMAX)							##
-            szaF       = np.zeros(ngeosMAX)							##
-            emissionF  = np.zeros(ngeosMAX)							##
-            betaF      = np.zeros(ngeosMAX)							##
-            azimuthF   = np.zeros(ngeosMAX)							##
-                                                ##
-            phaseF   [:ngeos] = np.repeat(phase[i], sum(A))					##
-            szaF     [:ngeos] = sza   [i,A]							##
-            emissionF[:ngeos] = emission[A]							##
-        betaF    [:ngeos] = beta  [i,A]							##
-        azimuthF [:ngeos] = phi   [i,A]							##
-                                                ##
-        filename = 'exopy/files/'+ files[scene]						##
-                                                ##
-            # Reading Stoke vector								##
-            Sv = geos.read_dap(filename, ngeos, phaseF, szaF, emissionF, azimuthF, betaF)	##
-                                                ##
-        I = Sv[0,:ngeos]/np.pi								##
-        Q = Sv[1,:ngeos]/np.pi								##
-        U = Sv[2,:ngeos]/np.pi								##
-        V = Sv[3,:ngeos]/np.pi								##
-        '''											##
-        ######################################################################################
-        ######################################################################################
+            # renormalizing
+            IQUV[0,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[0,A]*I/np.pi
+            IQUV[1,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[1,A]*Q/np.pi
+            IQUV[2,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[2,A]*U/np.pi
+            IQUV[3,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[3,A]*V/np.pi
 
+            # storing disk resolved case
+            body.grid.I[l,i,:] = IQUV[0,:]
+            body.grid.Q[l,i,:] = IQUV[1,:]
+            body.grid.U[l,i,:] = IQUV[2,:]
+            body.grid.V[l,i,:] = IQUV[3,:]
 
-        IQUV[0,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[0,A]*I/np.pi
-        IQUV[1,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[1,A]*Q/np.pi
-        IQUV[2,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[2,A]*U/np.pi
-        IQUV[3,A] = 4*np.cos(np.radians(sza[i,A]))*body.grid.shadow[i,A]*area[3,A]*V/np.pi
-
-        body.grid.I[i,:] = IQUV[0,:]
-        body.grid.Q[i,:] = IQUV[1,:]
-        body.grid.U[i,:] = IQUV[2,:]
-        body.grid.V[i,:] = IQUV[3,:]
-
-        Ip[i] = np.nansum(IQUV[0,:])
-        Qp[i] = np.nansum(IQUV[1,:])
-        Up[i] = np.nansum(IQUV[2,:])
-        Vp[i] = np.nansum(IQUV[3,:])
+            # storing disk integrated case
+            Ip[l,i] = np.nansum(IQUV[0,:])
+            Qp[l,i] = np.nansum(IQUV[1,:])
+            Up[l,i] = np.nansum(IQUV[2,:])
+            Vp[l,i] = np.nansum(IQUV[3,:])
 
         body.radiance.I = Ip
         body.radiance.Q = Qp
