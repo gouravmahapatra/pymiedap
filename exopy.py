958c875ce7d268af472b221aa7cc50b87d079afc
Loic Rossi <l.c.g.rossi@tudelft.nl>
2017-03-16 14:24:31 +0100

    Remove conflicting test in exopy.py
    Removed two lines that were for test purpose only

--- exopy.py
+++ exopy.py
@@ -10,7 +10,7 @@ Date: 2016-2017
 
 Model created in the framework of my master thesis work on exoplanets and exomoons.
 
-Recipe: 
+Recipe:
 
   1) Create a three body system.
   2) ...
@@ -39,7 +39,7 @@ _plt.rc('font', family='serif')
 # ======================================
 
 if __name__ == "__main__":
-    print(__doc__)  
+    print(__doc__)
 
 
 def test_integration(body, scene='clear', plot = 'no'):
@@ -52,9 +52,9 @@ def test_integration(body, scene='clear', plot = 'no'):
 
 	file1 = {'clear':'exopy/files/table_clear.dat','cloudy':'exopy/files/table_cloudy.dat','gas':'exopy/files/table_gas.dat'  }
 	file2 = {'clear':'exopy/files/clear_0.500.dat','cloudy':'exopy/files/cloudy_0.500.dat','gas':'exopy/files/fou_jup_100.dat'}
-	
+
 	t11 = t.time()
-	
+
 	time = body.ephemeris.time
 
 	Ip = np.zeros_like(time)
@@ -62,14 +62,14 @@ def test_integration(body, scene='clear', plot = 'no'):
 	Up = np.zeros_like(time)
 
 	output = np.zeros([4,len(body.ephemeris.time),body.grid.N_points])
-	
+
 	area  = np.repeat(body.grid.area[:,np.newaxis],4,1).T
 	phase = np.degrees(body.geometry.phase_angle)
 	sza   = np.degrees(body.grid.solar_zenith_angle)
 	emission = np.degrees(body.grid.observer_zenith_angle)
 	beta  = np.degrees(body.grid.beta)
     	phi   = np.degrees(body.grid.azimuth)
-	
+
 	for i,j in enumerate(time):
 	    t1 = t.time()
 	    print i+1, ' out of ', len(time)
@@ -128,7 +128,7 @@ def test_integration(body, scene='clear', plot = 'no'):
 
 		print t22-t11
 
-	
+
 	return [Ip, Qp, Up]
 
 
@@ -152,64 +152,7 @@ Parameters
 names : str/list
     name of the body(ies) to be created
 types : str/list
-    type of body(ies) to be created: 'planet', 'moon', 'star'   
-
-Returns
--------
-
-bodies : model.body object/list
-    Output generated body(ies)
-   
-Body object description
-----------------------
-    
-A body object serves as binder element for all the information related to \
-a celestial body. The following subclasses are associated to a body-type \
-object:
-    
-* Properties: contains the main physical properties of the body
-
-* Orbital elements: contains the orbital elements describing the orbit of the \
-body  
-    
-* Ephemeris: contains the position of each body for each epoch
-
-* Geometry: contains the angles defining the geometry for each epoch, as well \
-as the shadowing conditions
-
-* Flags: contains a series of flag indicators as a function of time
-
-* Grid: contains the grid properties for discretizing the body
-
-===============================================================================    
-    '''
-    import exopy_functions as fun
-    import numpy as np
-    
-    if type(names)==str:
-        bodies = fun.body(names, types)
-    else:        
-        bodies = list(np.zeros(len(names)))
-        for i in range(len(bodies)):
-            bodies[i] = fun.body(names[i], types[i])
-
-    return bodies
-
-
-def new_system(identifier):
-    '''
-===============================================================================
-  Function: model.new_system        November 2016, Javier B.M., TU Delft
--------------------------------------------------------------------------------
-
-The function creates a set of new body objects, based on a predefined system \
-stored in the 'model.py' file.
-
-Parameters
-----------
-
-identifier : str
-    identification string of the system of bodies to be loaded   
+    type of body(ies) to be created: 'planet', 'moon', 'star'
 
 Returns
 -------
@@ -238,36 +181,93 @@ as the shadowing conditions
 
 * Grid: contains the grid properties for discretizing the body
 
-===============================================================================    
-    '''    
+===============================================================================
+    '''
     import exopy_functions as fun
-    
+    import numpy as np
+
+    if type(names)==str:
+        bodies = fun.body(names, types)
+    else:
+        bodies = list(np.zeros(len(names)))
+        for i in range(len(bodies)):
+            bodies[i] = fun.body(names[i], types[i])
+
+    return bodies
+
+
+def new_system(identifier):
+    '''
+===============================================================================
+  Function: model.new_system        November 2016, Javier B.M., TU Delft
+-------------------------------------------------------------------------------
+
+The function creates a set of new body objects, based on a predefined system \
+stored in the 'model.py' file.
+
+Parameters
+----------
+
+identifier : str
+    identification string of the system of bodies to be loaded
+
+Returns
+-------
+
+bodies : model.body object/list
+    Output generated body(ies)
+
+Body object description
+----------------------
+
+A body object serves as binder element for all the information related to \
+a celestial body. The following subclasses are associated to a body-type \
+object:
+
+* Properties: contains the main physical properties of the body
+
+* Orbital elements: contains the orbital elements describing the orbit of the \
+body
+
+* Ephemeris: contains the position of each body for each epoch
+
+* Geometry: contains the angles defining the geometry for each epoch, as well \
+as the shadowing conditions
+
+* Flags: contains a series of flag indicators as a function of time
+
+* Grid: contains the grid properties for discretizing the body
+
+===============================================================================
+    '''
+    import exopy_functions as fun
+
     if identifier == 'transit':
-    
+
         Planet = fun.body('Planet', 'planet')
         Moon   = fun.body('Moon', 'moon')
         Star   = fun.body('Star', 'star')
-        
+
         Planet .properties.m = 5.972e24  # [kg]
         Moon.properties.m  = 7.342e22  # [kg]
         Star.properties.m   = 2e30      # [kg]
-        
+
         Planet .properties.R = 6.371008e6  # [m]
         Moon.properties.R  = 1.7374e6    # [m]
         Star.properties.R   = 6.957e8     # [m]
-        
+
         Moon.orbital_elements.a      = 3.844e8*5.9723e24/(5.972e24+7.342e22) # [m]
         Moon.orbital_elements.e      = 0#0.0549  # 0.077 # [-]
         Moon.orbital_elements.i      = 90   # [deg]
         Moon.orbital_elements.Omega  = 0     # [deg]
         Moon.orbital_elements.omega  = 90     # [deg]
-        
+
         Star.orbital_elements.a_b     = 1.5e11  # [m]
         Star.orbital_elements.e_b     = 0#0.0167  # [-]
         Star.orbital_elements.i_b     = 0#23.5    # [deg]
         Star.orbital_elements.Omega_b = 0.0     # [deg]
         Star.orbital_elements.omega_b = 0.0     # [deg]
-            
+
         bodies = [Moon, Planet , Star]
 
 
@@ -329,55 +329,55 @@ as the shadowing conditions
         bodies = [Moon, Planet, Star]
 
 
-        
+
     if identifier == 's_system':
-    
+
         Earth = fun.body('Earth', 'planet')
         Moon  = fun.body('Moon', 'moon')
         Sun   = fun.body('Sun', 'star')
-        
+
         Earth.properties.m = 5.972e24  # [kg]
         Moon.properties.m  = 7.342e22  # [kg]
         Sun.properties.m   = 2e30      # [kg]
-        
+
         Earth.properties.R = 6.371008e6  # [m]
         Moon.properties.R  = 1.7374e6    # [m]
         Sun.properties.R   = 6.957e8     # [m]
-        
+
         Moon.orbital_elements.a      = 3.844e8*5.9723e24/(5.972e24+7.342e22) # [m]
         Moon.orbital_elements.e      = 0.0549  # 0.077 # [-]
         Moon.orbital_elements.i      = 5.145   # [deg]
         Moon.orbital_elements.Omega  = 0.0     # [deg]
         Moon.orbital_elements.omega  = 0.0     # [deg]
-        
+
         Sun.orbital_elements.a_b     = 1.5e11  # [m]
         Sun.orbital_elements.e_b     = 0.0167  # [-]
         Sun.orbital_elements.i_b     = 23.5    # [deg]
         Sun.orbital_elements.Omega_b = 0.0     # [deg]
         Sun.orbital_elements.omega_b = 0.0     # [deg]
-            
+
         bodies = [Moon, Earth, Sun]
-    
+
     if identifier == 'pool_ball':
-        
+
         Earth = fun.body('Earth', 'planet')
         Moon  = fun.body('Moon', 'moon')
         Sun   = fun.body('Sun', 'star')
-        
+
         Earth.properties.m = 6e24  # [kg]
         Moon.properties.m  = 0     # [kg]
         Sun.properties.m   = 2e30  # [kg]
-        
+
         Earth.properties.R = 6.371e4  # [m]
         Moon.properties.R  = 1.737e6  # [m]
         Sun.properties.R   = 6.957e8  # [m]
-        
+
         Moon.orbital_elements.a      = 3.8e8  # [m]
         Moon.orbital_elements.e      = 0.055  # [-]
         Moon.orbital_elements.i      = 5.145  # [deg]
         Moon.orbital_elements.Omega  = 0.0    # [deg]
         Moon.orbital_elements.omega  = 0.0    # [deg]
-        
+
         Sun.orbital_elements.a_b     = 1e5    # [m]
         Sun.orbital_elements.e_b     = 0.017  # [-]
         Sun.orbital_elements.i_b     = 60     # [deg]
@@ -385,94 +385,94 @@ as the shadowing conditions
         Sun.orbital_elements.omega_b = 0.0    # [deg]
         #  60*60*24*1e-9,60*60*24*3e-7)
         bodies = [Moon, Earth, Sun]
-    
+
     if identifier == 'Moon_Earth_Sun_mod_eclipses_s_phase_meeting':
-    
+
         Earth = fun.body('Earth', 'planet')
         Moon  = fun.body('Moon', 'moon')
         Sun   = fun.body('Sun', 'star')
-        
+
         Earth.properties.m = 5.972e24  # [kg]
         Moon.properties.m  = 7.342e22  # [kg]
         Sun.properties.m   = 2e30      # [kg]
-        
+
         Earth.properties.R = 6.371e6  # [m]
         Moon.properties.R  = 1.737e6  # [m]
         Sun.properties.R   = 6.957e8  # [m]
-        
+
         Moon.orbital_elements.a      = 3.84748e8* 6e24/(6e24+7e22) # [m]
         Moon.orbital_elements.e      = 0 # [-]
         Moon.orbital_elements.i      = 90  # [deg]
         Moon.orbital_elements.Omega  = 0    # [deg]
         Moon.orbital_elements.omega  = 90    # [deg]
-        
+
         Sun.orbital_elements.a_b     = 1.5e11 # [m]
         Sun.orbital_elements.e_b     = 0  # [-]
         Sun.orbital_elements.i_b     = 0     # [deg]
         Sun.orbital_elements.Omega_b = 0   # [deg]
-        Sun.orbital_elements.omega_b = 0    # [deg]      
-    
+        Sun.orbital_elements.omega_b = 0    # [deg]
+
         bodies = [Moon, Earth, Sun]
-    
+
     if identifier == 'Moon_Earth_Sun_mod_eclipses_s_meeting':
-    
+
         Earth = fun.body('Earth', 'planet')
         Moon  = fun.body('Moon', 'moon')
         Sun   = fun.body('Sun', 'star')
-        
+
         Earth.properties.m = 5.972e24  # [kg]
         Moon.properties.m  = 7.342e22  # [kg]
         Sun.properties.m   = 2e30      # [kg]
-        
+
         Earth.properties.R = 6.371e6  # [m]
         Moon.properties.R  = 1.737e6  # [m]
         Sun.properties.R   = 6.957e8  # [m]
-        
+
         Moon.orbital_elements.a      = 3.84748e8* 6e24/(6e24+7e22) # [m]
         Moon.orbital_elements.e      = 0.5 # [-]
         Moon.orbital_elements.i      = 0  # [deg]
         Moon.orbital_elements.Omega  = 0    # [deg]
         Moon.orbital_elements.omega  = 180    # [deg]
-        
+
         Sun.orbital_elements.a_b     = 1.5e11 # [m]
         Sun.orbital_elements.e_b     = 0  # [-]
         Sun.orbital_elements.i_b     = 50     # [deg]
         Sun.orbital_elements.Omega_b = 0   # [deg]
-        Sun.orbital_elements.omega_b = 300    # [deg]  
-        
+        Sun.orbital_elements.omega_b = 300    # [deg]
+
         bodies = [Moon, Earth, Sun]
-        
+
     if identifier == 'Moon_Earth_Sun_mod_eclipses_ob_meeting':
-        
+
         Earth = fun.body('Earth', 'planet')
         Moon  = fun.body('Moon', 'moon')
         Sun   = fun.body('Sun', 'star')
-        
+
         Earth.properties.m = 5.972e24  # [kg]
         Moon.properties.m  = 7.342e22  # [kg]
         Sun.properties.m   = 2e30      # [kg]
-        
+
         Earth.properties.R = 6.371e6  # [m]
         Moon.properties.R  = 1.737e6  # [m]
         Sun.properties.R   = 6.957e8  # [m]
-        
+
         Moon.orbital_elements.a      = 3.84748e8* 6e24/(6e24+7e22) # [m]
         Moon.orbital_elements.e      = 0 # [-]
         Moon.orbital_elements.i      = 0  # [deg]
         Moon.orbital_elements.Omega  = 0    # [deg]
         Moon.orbital_elements.omega  = -1.3    # [deg]
-        
+
         Sun.orbital_elements.a_b     = 1.5e11 # [m]
         Sun.orbital_elements.e_b     = 0  # [-]
         Sun.orbital_elements.i_b     = 90     # [deg]
         Sun.orbital_elements.Omega_b = 0   # [deg]
-        Sun.orbital_elements.omega_b = 89.73    # [deg]          
-        
+        Sun.orbital_elements.omega_b = 89.73    # [deg]
+
         bodies = [Moon, Earth, Sun]
-        
-        
+
+
     return bodies
-    
+
 
 def run_simulation(body1, body2 ,star,dt, tf, t0_moon=0, t0_planet=0):
     if body1.type == 'moon':
@@ -484,9 +484,7 @@ def run_simulation(body1, body2 ,star,dt, tf, t0_moon=0, t0_planet=0):
     compute.transits([body1, body2, star])
     compute.eclipses([body1,body2], star)
 
-    body1.atmosphere.name = 'Hola'
-    body2.atmosphere.name = 'Adios'
-    compute.int_radiance([body1, body2])    
+    compute.int_radiance([body1, body2])
     I,Q,U,V = compute.combine([body1, body2])
 
     return I,Q,U,V
