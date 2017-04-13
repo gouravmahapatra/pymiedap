Fix normalization constant in planet integrated

pymiedap.py
@@ -2006,10 +2006,10 @@ def planet_integrated(models, alpha=[10], npix=15, force=False, set_taus=False,
                 #===============
 
                 # Integrating over planet
-                Iall[:,a,citer] = 4*np.nansum(Ix,axis=1)*apix/np.pi
-                Qall[:,a,citer] = 4*np.nansum(Qx,axis=1)*apix/np.pi
-                Uall[:,a,citer] = 4*np.nansum(Ux,axis=1)*apix/np.pi
-                Vall[:,a,citer] = 4*np.nansum(Vx,axis=1)*apix/np.pi
+                Iall[:,a,citer] = np.nansum(Ix,axis=1)*apix
+                Qall[:,a,citer] = np.nansum(Qx,axis=1)*apix
+                Uall[:,a,citer] = np.nansum(Ux,axis=1)*apix
+                Vall[:,a,citer] = np.nansum(Vx,axis=1)*apix
 
             else:
                 # save some information in the model