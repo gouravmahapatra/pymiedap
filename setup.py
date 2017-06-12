df3094c9c23899646cf83291e45b1bc6ff9ebf14
Loic Rossi <loic.cg.rossi@gmail.com>
2017-06-12 11:23:54 +0200

    Updated the setup file for the install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from numpy.distutils.core import setup, Extension
 import os
 import os.path as osp
 
-module_mie = Extension(name = 'pymiedap.module_mie',
+module_mie = Extension(name = 'module_mie',
                        sources=['./mie_sig.pyf',
                                 './mie_source/anbn.f',
                                 './mie_source/devel.f',
@@ -13,18 +13,17 @@ module_mie = Extension(name = 'pymiedap.module_mie',
                                 './mie_source/gauleg.f',
                                 './mie_source/getin.f',
                                 './mie_source/pitau.f',
-                                './mie_source/rdwavfile.f',
                                 './mie_source/rminmax.f',
                                 './mie_source/scatmat.f',
                                 './mie_source/sizedis.f',
                                 './mie_source/writsc.f',
                                 ])
 
-module_read_mie = Extension(name = 'pymiedap.module_readmie',
+module_readmie = Extension(name = 'module_readmie',
                             sources=['./readmie_sig.pyf',
                                      './readmie_source/read_mie_output.f', ])
 
-module_mieshell = Extension(name = 'pymiedap.module_mieshell',
+module_mieshell = Extension(name = 'module_mieshell',
                             sources=['./mieshell_sig.pyf',
                                      './mieshell_source/bhcoat.f',
                                      './mieshell_source/gauleg.f',
@@ -33,7 +32,7 @@ module_mieshell = Extension(name = 'pymiedap.module_mieshell',
                                      './mieshell_source/sizedis.f',
                                      ])
 
-module_dap = Extension(name = 'pymiedap.module_dap',
+module_dap = Extension(name = 'module_dap',
                        sources=['./dap_sig.pyf',
                                 './dap_source/adding.f',
                                 './dap_source/addlay.f',
@@ -44,7 +43,6 @@ module_dap = Extension(name = 'pymiedap.module_dap',
                                 './dap_source/brack.f',
                                 './dap_source/bstart.f',
                                 './dap_source/double.f',
-                                './dap_source/endfou.f',
                                 './dap_source/expbmu.f',
                                 './dap_source/fillup.f',
                                 './dap_source/gauleg.f',
@@ -71,7 +69,7 @@ module_dap = Extension(name = 'pymiedap.module_dap',
                                 './dap_source/tstar.f',
                                 ])
 
-module_geos = Extension(name = 'pymiedap.module_geos',
+module_geos = Extension(name = 'module_geos',
                         sources=['./geos_sig.pyf',
                                  './geos_source/bracks.f',
                                  './geos_source/getgeos.f',
@@ -90,7 +88,7 @@ setup(name='PyMieDAP',
       author='Loic Rossi, Daphne Stam',
       author_email='l.c.g.rossi@tudelft.nl',
       license='GPL/CeCILL',
-      packages=['pymiedap'],
+      packages=['pymiedap','pymiedap.exopy_source'],
       ext_modules=[module_mie, module_readmie, module_mieshell, module_dap, module_geos],
       zip_safe=False,
       )
